dcbf3123d27ac5c18bd869cf49bc4f061ae32f9c
sumeya2235 <sumur2235@gmail.com>
2025-02-27 16:16:48 -0800

    banner

--- netflix-clone/src/utils/axios.js
+++ netflix-clone/src/utils/axios.js
@@ -1,5 +1,9 @@
+// src/utils/axios.js
 import axios from "axios";
+
+// Base URL to make requests to the TMDb
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
 });
+
 export default instance;
